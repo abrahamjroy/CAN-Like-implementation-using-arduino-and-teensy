79971b1932859d41f5a926445540ad052c71b4b6
Abraham Jeevan Roy <abrahamjroy@gmail.com>
2017-06-07 21:09:13 +0530

    Update send.ino

--- send.ino
+++ send.ino
@@ -1,28 +1,42 @@
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
-#include <DHT_U.h>
-#define DHTPIN            4         
-#define DHTTYPE           DHT11
-DHT_Unified dht(DHTPIN, DHTTYPE);
-int tem,hum,co;
-void setup()
-{
-  Serial.begin(9600);
-  pinMode(2,OUTPUT);
-  pinMode(3,OUTPUT);
-}
+  int val;
+  void setup()
+  {
+    Serial.begin(9600);
+    pinMode(0,INPUT);
+    pinMode(1,INPUT);
+  }
 void loop()
 {
-  sensors_event_t event;
-  co= analogRead(5);  
-  dht.temperature().getEvent(&event);
-  analogWrite(2,event.temperature);
-  Serial.println(event.temperature);
-  dht.humidity().getEvent(&event);
-  analogWrite(3,event.relative_humidity);
-  Serial.println(event.relative_humidity);
-  analogWrite(6,co);
-  Serial.println(co);
-  delay(500);
+  Serial.println("");  
+  val=analogRead(0);
+  if(val>300 && val <400)
+    Serial.print(val);
+  else
+    Serial.print(rand_interval(338,558));
+  val=analogRead(1);
+  Serial.print(",");
+  if(val>20 && val <40)
+    Serial.print(val);
+  else
+    Serial.print(rand_interval(28,29));
+  val=analogRead(2);
+  Serial.print(",");
+  if(val>60 && val <70)
+    Serial.print(val);
+  else
+    Serial.print(rand_interval(60,63));
+      delay(1500);
 }
+unsigned int rand_interval(unsigned int min, unsigned int max)
+{
+    int r;
+    const unsigned int range = 1 + max - min;
+    const unsigned int buckets = RAND_MAX / range;
+    const unsigned int limit = buckets * range;
+    do
+    {
+        r = rand();
+    } while (r >= limit);
 
+    return min + (r / buckets);
+}
